496dcda1b9def2e48a7b215f8063809f8d804bd3
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-10-18 17:54:49 +0800

    ch-7: transform operators

--- ToyRxPlayground/main.swift
+++ ToyRxPlayground/main.swift
@@ -13,186 +13,140 @@ enum MyError: Error {
     case anError
 }
 
-example(of: "ignoreElements") {
-    let strikes = PublishSubject<String>()
-    let disposeBag = DisposeBag()
+example(of: "toArray") {
+    let bag = DisposeBag()
     
-    strikes
-        .ignoreElements()
-        .subscribe({ _ in
-            print("you're out!")
-        })
-        .disposed(by: disposeBag)
-    
-    strikes.onNext("X")
-    strikes.onNext("X")
-    strikes.onNext("X")
-    strikes.onCompleted()
-}
-
-example(of: "elementAt") {
-    let strikes = PublishSubject<String>()
-    let disposeBag = DisposeBag()
-    
-    strikes
-        .elementAt(2)
-        .subscribe(onNext: {_ in
-            print("you're out!")
-        })
-        .disposed(by: disposeBag)
-    
-    strikes.onNext("1")
-    strikes.onNext("2")
-    strikes.onNext("3")
-}
-
-example(of: "filter") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(1, 2, 3, 4, 5, 6)
-        .filter { $0.isMultiple(of: 2) }
-        .subscribe(onNext: {
+    Observable.of("a", "b", "c")
+        .toArray()
+        .subscribe(onSuccess: {
             print($0)
         })
-        .disposed(by: disposeBag)
+        .disposed(by: bag)
 }
 
-example(of: "skip") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of("A", "B", "C", "D", "E", "F")
-        .skip(3)
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "skipWhile") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(2, 2, 3, 4, 4)
-        .skipWhile { $0.isMultiple(of: 2) }
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "skipUntil") {
-    let disposeBag = DisposeBag()
-    
-    let subject = PublishSubject<String>()
-    let trigger = PublishSubject<String>()
-    
-    subject
-        .skipUntil(trigger)
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-    
-    subject.onNext("A")
-    subject.onNext("B")
-    
-    trigger.onNext("X")
-    
-    subject.onNext("C")
-}
-
-example(of: "take") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(1, 2, 3, 4, 5, 6)
-        .take(3)
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "takeWhile") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(2, 2, 4, 4, 6, 6)
-        .enumerated()
-        .takeWhile({ index, integer in
-            integer.isMultiple(of: 2) && index < 3
-        })
-        .map(\.element)
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "takeUntil") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of(1, 2, 3, 4, 5)
-        .takeUntil(.inclusive) { $0.isMultiple(of: 4) }
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "takeUntil trigger") {
-    let disposeBag = DisposeBag()
-    
-    let subject = PublishSubject<String>()
-    let trigger = PublishSubject<String>()
-    
-    subject
-        .takeUntil(trigger)
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-    
-    subject.onNext("1")
-    subject.onNext("2")
-    
-    trigger.onNext("X")
-    
-    subject.onNext("3")
-}
-
-example(of: "distinctUntilChanged") {
-    let disposeBag = DisposeBag()
-    
-    Observable.of("A", "A", "B", "B", "A")
-        .distinctUntilChanged()
-        .subscribe(onNext: {
-            print($0)
-        })
-        .disposed(by: disposeBag)
-}
-
-example(of: "distinctUntilChanged(_:)") {
-    let disposeBag = DisposeBag()
+example(of: "map") {
+    let bag = DisposeBag()
     
     let formatter = NumberFormatter()
     formatter.numberStyle = .spellOut
     
-    Observable<NSNumber>.of(10, 110, 20, 200, 210, 310)
-        .distinctUntilChanged { a, b in
-            guard let aWords = formatter.string(from: a)?.components(separatedBy: " "),
-                  let bWords = formatter.string(from: b)?.components(separatedBy: " ") else {
-                return false
-            }
-            
-            var containsMatch = false
-            
-            for aWord in aWords where bWords.contains(aWord) {
-                containsMatch = true
-                break
-            }
-            
-            return containsMatch
+    Observable<Int>.of(123, 4, 56)
+        .map {
+            formatter.string(for: $0) ?? ""
         }
         .subscribe(onNext: {
             print($0)
         })
-        .disposed(by: disposeBag)
+        .disposed(by: bag)
+}
+
+example(of: "enumerated and map") {
+    let bag = DisposeBag()
+    
+    Observable.of(1, 2, 3, 4, 5, 6)
+        .enumerated()
+        .map { index, integer in
+            index > 2 ? integer * 2 : integer
+        }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: bag)
+}
+
+example(of: "compactMap") {
+    let bag = DisposeBag()
+    
+    Observable.of("to", "be", nil, "or", "not", "to", "be", nil)
+        .compactMap { $0 }
+        .toArray()
+        .map({ $0.joined(separator: " " )})
+        .subscribe(onSuccess: {
+            print($0)
+        })
+        .disposed(by: bag)
+}
+
+struct Student {
+    let score: BehaviorSubject<Int>
+}
+
+example(of: "flatMap") {
+    let bag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+    
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMap { $0.score }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: bag)
+    
+    student.onNext(laura)
+    laura.score.onNext(85)
+    
+    student.onNext(charlotte)
+    charlotte.score.onNext(100)
+}
+
+example(of: "flatMapLatest") {
+    let bag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 90))
+    
+    let student = PublishSubject<Student>()
+    
+    student
+        .flatMapLatest { $0.score }
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: bag)
+    
+    student.onNext(laura)
+    laura.score.onNext(85)
+    student.onNext(charlotte)
+    
+    laura.score.onNext(95)
+    charlotte.score.onNext(100)
+}
+
+example(of: "materialize and dematerialize") {
+    let bag = DisposeBag()
+    
+    let laura = Student(score: BehaviorSubject(value: 80))
+    let charlotte = Student(score: BehaviorSubject(value: 100))
+    
+    let student = BehaviorSubject(value: laura)
+    
+    let studentScore = student
+        .flatMapLatest {
+            $0.score.materialize()
+        }
+    
+    studentScore
+        .filter {
+            guard $0.error == nil else {
+                print($0.error!)
+                return false
+            }
+            
+            return true
+        }
+        .dematerialize()
+        .subscribe(onNext: {
+            print($0)
+        })
+        .disposed(by: bag)
+    
+    laura.score.onNext(85)
+    laura.score.onError(MyError.anError)
+    laura.score.onNext(90)
+    
+    student.onNext(charlotte)
 }
